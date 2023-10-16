653822f52e64e3f15864bcc92dc3b3d3820d19d0
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-10-16 16:16:58 -0700

    backend structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+cats = [
+  {
+    name: 'Franklin',
+    age: 5,
+    hobbies: 'eating toilet paper',
+    image: 'https://unsplash.com/photos/7DXQtl__9Cw'
+  },
+  {
+    name: 'Solo',
+    age: 2,
+    hobbies: 'loves the scratching post',
+    image: 'https://unsplash.com/photos/a-cat-sitting-on-t
+    op-of-a-cat-tree-hgGZPMWaKjA'
+  },
+  {
+    name: 'Belinda',
+    age: 10,
+    hobbies: 'biting people face while asleep',
+    image: 'https://unsplash.com/photos/uhnbTZC7N9k'
+  }
+]
+
+cats.each do |each_cat|
+  Cat.create each_cat
+  puts "creating cat #{each_cat}"
+end
