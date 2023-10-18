aff836acbd3b5f953f293872af64ed1f2adc2952
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-10-18 05:30:51 -0700

    completed api-endpoint challenges

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -12,20 +12,23 @@ class CatsController < ApplicationController
         end
     end
     
+    def update
+    cat = Cat.find(params[:id])
+    cat.update(cat_params)
+    if cat.valid?
+      render json: cat
+    else
+      render json: { errors: cat.errors.full_messages }, status: 422
+    end
+  end
       
-      def update
-        cat = Cat.find(params[:id])
-        cat.update(cat_params)
-        if cat.valid?
-            render json: cat
-        end
-      end
-      
-      def destroy
-      end
-      
-      private
-      def cat_params
-          params.require(:cat).permit(:name, :age, :enjoys, :image)
-      end
+  def destroy
+    cat = Cat.find(params[:id])
+    cat.destroy
+    head :no_content
+  end
+   private
+    def cat_params
+        params.require(:cat).permit(:name, :age, :hobbies, :image)
+    end
 end
--- spec/requests/cats_request_spec.rb
+++ spec/requests/cats_request_spec.rb
@@ -17,54 +17,64 @@ RSpec.describe "Cats", type: :request do
       expect(cat.length).to eq 1
     end
   end
-
   describe "POST /create" do
-    it "creates a cat" do
-      # The params we are going to send with the request
+    it 'creates a cat' do
       cat_params = {
         cat: {
-          name: 'Solo',
-          age: 2,
-          hobbies: 'loves the scratching post',
-          image: 'https://unsplash.com/photos/a-cat-sitting-on-t
-          op-of-a-cat-tree-hgGZPMWaKjA'
+          name: 'Franklin',
+          age: 5,
+          hobbies: 'eating toilet paper',
+          image: 'https://unsplash.com/photos/7DXQtl__9Cw'
         }
       }
       post '/cats', params: cat_params
       expect(response).to have_http_status(200)
       cat = Cat.first
-      expect(cat.name).to eq 'Solo'
+      expect(cat.name).to eq 'Franklin'
+      expect(cat.age).to eq 5
+      expect(cat.hobbies).to eq 'eating toilet paper'
     end
   end
 
-    describe "PATCH /update" do
-        it "updates an existing cat" do
-        cat_params = {
-            cat: {
-                name: 'Solo',
-                age: 2,
-                hobbies: 'loves the scratching post',
-                image: 'https://unsplash.com/photos/a-cat-sitting-on-top-of-a-cat-tree-hgGZPMWaKjA'
-            }
-         }
+  describe "PATCH /update/:id" do
+    it 'updates a cat' do
+      cat = Cat.create(
+        name: 'Felix',
+        age: 3,
+        hobbies: 'chasing mice',
+        image: 'https://example.com/cat.jpg'
+      )
 
-        post '/cats', params: cat_params
-        cat = Cat.first
-
-        updated_cat_params = {
-            cat: {
-                name: 'Solo',
-                age: 3,
-                hobbies: 'loves the scratching post',
-                image: 'https://unsplash.com/photos/a-cat-sitting-on-top-of-a-cat-tree-hgGZPMWaKjA'
-            }
+      updated_cat_params = {
+        cat: {
+          name: 'Whiskers',
+          age: 4,
+          hobbies: 'napping',
+          image: 'https://example.com/whiskers.jpg'
         }
+      }
 
-        patch '/cats/#{cat.id}', params: updated_cat_params
-        updated_cat = cat.find(cat.id)
-
-        expect(response).to have_http_status(200)
-        expect(updated_cat.age).to eq 3
-        end
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      expect(response).to have_http_status(200)
+      cat.reload
+      expect(cat.name).to eq 'Whiskers'
+      expect(cat.age).to eq 4
+      expect(cat.hobbies).to eq 'napping'
     end
+  end
+
+  describe "DELETE /destroy/:id" do
+    it 'deletes a cat' do
+      cat = Cat.create(
+        name: 'Mittens',
+        age: 2,
+        hobbies: 'playing with yarn',
+        image: 'https://example.com/mittens.jpg'
+      )
+
+      delete "/cats/#{cat.id}"
+      expect(response).to have_http_status(204)
+      expect { cat.reload }.to raise_error ActiveRecord::RecordNotFound
+    end
+  end
 end
